92788b99608089957648eb5b8c8426f2d018fae8
Xenon-Ware <116182930+Dinico414@users.noreply.github.com>
2026-03-26 19:45:18 +0100

    improove pixelwatch aod ui

--- app/src/main/java/com/xenonware/cloudremote/CurtainActivity.kt
+++ app/src/main/java/com/xenonware/cloudremote/CurtainActivity.kt
@@ -9,17 +9,29 @@ import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color as ComposeColor
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.xenonware.cloudremote.ui.res.PixelWatchFace
+import kotlinx.coroutines.delay
 
 class CurtainActivity : ComponentActivity() {
 
@@ -33,7 +45,7 @@ class CurtainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         // Disable back press
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
@@ -45,19 +57,34 @@ class CurtainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
+            var isActive by remember { mutableStateOf(true) }
+
+            val animatedTextAlpha by animateFloatAsState(
+                targetValue = if (isActive) 0.5f else 0f,
+                label = "textAlpha",
+                animationSpec = tween(durationMillis = 500)
+            )
+
+            LaunchedEffect(isActive) {
+                if (isActive) {
+                    delay(10000)
+                    isActive = false
+                }
+            }
+
             Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(ComposeColor.Black),
                 contentAlignment = Alignment.Center
             ) {
-                PixelWatchFace()
+                PixelWatchFace(isActive = isActive)
             }
         }
 
         // Hide UI after content is set to ensure DecorView is ready
         hideSystemUI()
-        
+
         val filter = IntentFilter(ACTION_CLOSE_CURTAIN)
         ContextCompat.registerReceiver(this, receiver, filter, ContextCompat.RECEIVER_NOT_EXPORTED)
     }
@@ -77,7 +104,8 @@ class CurtainActivity : ComponentActivity() {
 
     private fun hideSystemUI() {
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
-        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 
