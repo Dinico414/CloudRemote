736b1ed256bb4703f90aa1fdc601ca34ddced46b
Xenon-Ware <116182930+Dinico414@users.noreply.github.com>
2026-04-03 19:27:15 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ configure<ApplicationExtension> {
             minSdk = 29
             targetSdk = 36
             versionCode = 1
-            versionName = "1.2.7"
+            versionName = "1.2.8"
 
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             buildConfigField("String", "XENON_COMMONS_VERSION", "\"${libs.versions.xenonCommons.get()}\"")
