cd084a9f207fdce8462cbcaa45fb1357df1504b2
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2026-04-09 02:34:31 +0200

    fix missing icons

--- app/src/main/java/com/xenonware/cloudremote/widget/ConnectedDevicesWidget.kt
+++ app/src/main/java/com/xenonware/cloudremote/widget/ConnectedDevicesWidget.kt
@@ -243,7 +243,7 @@ class ConnectedDevicesWidget : GlanceAppWidget() {
         val contentColor = if (item.battery > 20) textColor else GlanceTheme.colors.onSurface
 
         // Icon resource for BT device type
-// Inside ConnectedDevicesWidget -> DeviceBatteryBar
+
         val typeIconRes = when {
             // 1. Check Local Device Identity/Name first
             item.isLocal -> {
@@ -254,7 +254,6 @@ class ConnectedDevicesWidget : GlanceAppWidget() {
                     name.contains("Fold", ignoreCase = true) -> R.drawable.rounded_devices_fold_24
                     name.contains("Tablet", ignoreCase = true) -> R.drawable.rounded_tablet_24
                     name.contains("TV", ignoreCase = true) -> R.drawable.rounded_tv_gen_24
-                    // Specific phones as phone_android
                     name.contains("LG Wing", ignoreCase = true) ||
                             name.contains("iKKO Mind One", ignoreCase = true) ||
                             name.contains("Clicks Communicator", ignoreCase = true) ||
@@ -265,19 +264,21 @@ class ConnectedDevicesWidget : GlanceAppWidget() {
             // 2. Bluetooth Device Type Mapping
             item.type != null -> {
                 when (item.type) {
-                    BTDeviceType.PHONE -> R.drawable.round_phone_android_24
-                    BTDeviceType.TV -> R.drawable.rounded_tv_gen_24
-                    BTDeviceType.WATCH -> R.drawable.round_watch_24
-                    BTDeviceType.COMPUTER, BTDeviceType.LAPTOP -> R.drawable.rounded_monitor_24
                     BTDeviceType.HEADSET, BTDeviceType.EARBUDS -> R.drawable.round_headphones_24
+                    BTDeviceType.WATCH -> R.drawable.round_watch_24
                     BTDeviceType.KEYBOARD -> R.drawable.round_keyboard_24
                     BTDeviceType.MOUSE -> R.drawable.round_mouse_24
                     BTDeviceType.SPEAKER -> R.drawable.round_speaker_24
                     BTDeviceType.CONTROLLER -> R.drawable.round_gamepad_24
-                    else -> R.drawable.rounded_monitor_24 // "Monitor is for everything else"
+                    BTDeviceType.PEN -> R.drawable.round_edit_24
+                    BTDeviceType.HEARING_AID -> R.drawable.round_hearing_24
+                    BTDeviceType.PHONE -> R.drawable.round_phone_android_24
+                    BTDeviceType.COMPUTER, BTDeviceType.LAPTOP -> R.drawable.round_computer_24
+                    BTDeviceType.TV -> R.drawable.round_tv_24
+                    else -> R.drawable.round_bluetooth_24
                 }
             }
-            else -> R.drawable.rounded_monitor_24
+            else -> R.drawable.round_bluetooth_24
         }
 
         Box(
